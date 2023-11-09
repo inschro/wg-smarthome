e1f34f8041f1946ae871201dfbc18d0afedf575b
Ingo Schröder <inschro@gmail.com>
2023-11-09 17:02:55 +0100

    playing aroung with ipv6

--- components/ControlItem.tsx
+++ components/ControlItem.tsx
@@ -17,10 +17,22 @@ const ControlItem: React.FC<ControlItemProps> = ({
         "on": true
       })
     })
+    await fetch('http://[fe80::eeb5:faff:fe98:2403]/api/GWl0EP03SM0hqgcMfwoL-r0eiRyXM5ZgK18i-sS5/lights/2/state', {
+      method: 'put',
+      body: JSON.stringify({
+        "on": true
+      })
+    })
   }
 
   const lightOff = async () => {
     await fetch('http://192.168.0.232/api/GWl0EP03SM0hqgcMfwoL-r0eiRyXM5ZgK18i-sS5/lights/1/state', {
+      method: 'put',
+      body: JSON.stringify({
+        "on": false
+      })
+    })
+    await fetch('http://[fe80::eeb5:faff:fe98:2403]/api/GWl0EP03SM0hqgcMfwoL-r0eiRyXM5ZgK18i-sS5/lights/2/state', {
       method: 'put',
       body: JSON.stringify({
         "on": false
