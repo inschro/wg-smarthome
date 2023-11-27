901e558be7ee58f2291a2867da12556d9d4c64b3
Ingo Schröder <inschro@gmail.com>
2023-11-27 18:26:34 +0100

    light on dynamics

--- functions/lightControls.ts
+++ functions/lightControls.ts
@@ -1,24 +1,7 @@
 
 
-export const setLightOff = async (id: string) => {
-  // set brightness to 0%
-  await fetch("/api/hue", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      "endpoint": `/resource/light/${id}`
-    },
-    body: JSON.stringify({
-      "dimming": {
-        "brightness": 0,
-      },
-      "dynamics": {
-        "duration": 10000,
-      }
-    })
-  })
-  // set light off
-  await fetch("/api/hue", {
+export const setLightOff = (id: string) => {
+  fetch("/api/hue", {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
@@ -26,12 +9,15 @@ export const setLightOff = async (id: string) => {
     },
     body: JSON.stringify({
       "on": { "on": false },
+      "dynamics": {
+        "duration": 1000,
+      }
     })
   })
 }
 
-export const setLightOn = async (id: string) => {
-  await fetch("/api/hue", {
+export const setLightOn = (id: string) => {
+  fetch("/api/hue", {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
@@ -40,23 +26,10 @@ export const setLightOn = async (id: string) => {
     body: JSON.stringify({
       "on": { "on": true },
       "dimming": {
-        "brightness": 0,
-      }
-    })
-  })
-  //set brightness to 100%
-  await fetch("/api/hue", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      "endpoint": `/resource/light/${id}`
-    },
-    body: JSON.stringify({
-      "dimming": {
         "brightness": 100,
       },
       "dynamics": {
-        "duration": 10000,
+        "duration": 1000,
       }
     })
   })
