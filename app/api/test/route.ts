1ca2b615414b28185f6ab2c215834e4b00b99a7d
Ingo Schröder <inschro@gmail.com>
2023-11-29 23:13:44 +0100

    naming fix

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,7 +2,5 @@ import fs from 'fs'
 
 export async function GET(req: Request) {
 
-    const data = await fs.promises.readFile('GPTTemplates/testSystemMessage.txt', 'utf8')
-
-    return new Response(data)
+    return new Response("data")
 }
