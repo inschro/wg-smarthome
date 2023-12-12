2de4486167c0471a76bbcf99add2b0cc8cabdc9e
Ingo Schröder <inschro@gmail.com>
2023-12-12 06:39:39 +0100

    test edeka ebon endpoint added

---
+++ app/api/ebon/edeka/route.ts
@@ -0,0 +1,41 @@
+/*
+//function that sends the pdf to the api. attachment is a gmail attachment object of the pdf
+function sendPdfToApi(attachment) {
+  const apiEndpoint = '<this endpoint>';
+  const payload = {
+    filename: attachment.getName(),
+    mimeType: attachment.getContentType(),
+    content: Utilities.base64Encode(attachment.getBytes())
+  };
+
+  const options = {
+    'method' : 'post',
+    'contentType': 'application/json',
+    'payload' : JSON.stringify(payload)
+  };
+
+  UrlFetchApp.fetch(apiEndpoint, options);
+}
+*/
+export async function POST(req: Request) {
+
+  console.log("POST request received.")
+
+  return { status: 200 };
+  if (req.headers.get('Content-Type') !== 'application/json') {
+    return { status: 400, body: 'Bad Request, Content-Type must be application/json' };
+  }
+
+  const body = await req.json();
+
+  if (!body.filename || !body.mimeType || !body.content) {
+    return { status: 400, body: 'Bad Request' };
+  }
+
+  const { filename, mimeType, content } = body;
+
+  const pdfBuffer = Buffer.from(content, 'base64');
+
+
+  return { status: 200 };
+}
