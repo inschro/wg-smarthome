e5694266d344ab6e4c1e8a7944d890918cbe0e72
Ingo Schröder <inschro@gmail.com>
2023-12-17 09:25:03 +0100

    working on route handling

--- app/api/ebon/edeka/route.ts
+++ app/api/ebon/edeka/route.ts
@@ -1,27 +1,31 @@
 
 
 export async function POST(req: Request) {
+  try{
+    const isAuthenticated = checkAuthentication(req)
+    if (!isAuthenticated) {
+      return new Response("Unauthorized, Authentication token is incorrect", { status: 401 }) 
+    }
+    
+    if (req.headers.get('Content-Type') !== 'application/json') {
+      return new Response("Bad Request, Content-Type must be application/json", { status: 400 })
+    }
 
-  const isAuthenticated = checkAuthentication(req)
-  if (!isAuthenticated) {
-    return new Response("Unauthorized, Authentication token is incorrect", { status: 401 }) 
+    const body = await req.json()
+
+    if (!body.filename || !body.mimeType || !body.content) {
+      return new Response("Bad Request, missing filename, mimeType or content", { status: 400 })
+    }
+
+    const { filename, mimeType, content } = body
+
+    const pdfBuffer = Buffer.from(content, 'base64')
+
+    return new Response("check")
+  } catch (e) {
+    return new Response("An error occured", { status: 500 })
   }
   
-  if (req.headers.get('Content-Type') !== 'application/json') {
-    return new Response("Bad Request, Content-Type must be application/json", { status: 400 })
-  }
-
-  const body = await req.json()
-
-  if (!body.filename || !body.mimeType || !body.content) {
-    return new Response("Bad Request, missing filename, mimeType or content", { status: 400 })
-  }
-
-  const { filename, mimeType, content } = body
-
-  const pdfBuffer = Buffer.from(content, 'base64')
-
-  return new Response("check")
 }
 
 function checkAuthentication(req: Request): boolean {
